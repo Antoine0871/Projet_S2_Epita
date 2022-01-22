5e4b86c8ace43992744c6f9ec73549dfcff05c18
Antoine0871 <antoine.riquet01@orange.fr>
2022-01-22 20:51:17 +0000

    adding exe

--- Hanabi/Assets/Scripts/Multi/playerList.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class playerList : ScriptableObject
-{
-    [SerializeField] public List<GameObject> players = new List<GameObject>();
-}
